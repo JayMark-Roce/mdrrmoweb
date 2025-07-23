c9622ed645810058f3928dc9c7927c8af5a16a53
JayMark-Roce <jaymroce@gmail.com>
2025-07-24 02:35:15 +0800

    gps

--- resources/views/public/partials/navbar.blade.php
+++ resources/views/public/partials/navbar.blade.php
@@ -6,5 +6,9 @@
         <li><a href="{{ url('/') }}" class="hover:underline">Home</a></li>
         <li><a href="{{ url('/contact') }}" class="hover:underline">Contact</a></li>
         <li><a href="{{ url('/services') }}" class="hover:underline">Services</a></li>
+        <li>    <a href="{{ url('/admin/posting') }}" class="hover:underline">Posting</a>           </li>
+        <li>    <a href="{{ url('/admin/gps') }}" class="hover:underline">GPS Tracker</a>           </li>
+        <li>    <a href="{{ url('/admin/ambulances') }}" class="hover:underline">Ambulances</a></li>
+        <li>    <a href="{{ url('/admin/drivers') }}" class="hover:underline">Drivers</a>           </li>
     </ul>
 </nav>
